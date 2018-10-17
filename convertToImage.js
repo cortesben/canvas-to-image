2ec5e70d9b90aa532e90e184f5943f2114b1fdde
Glitch (canvas-to-image) <none>
2018-10-17 18:30:40 +0000

    🎎👢 Updated with Glitch

--- convertToImage.js
+++ convertToImage.js
@@ -1,32 +1,65 @@
-const s_canvasImage = document.querySelector('.image-1');
-const s_imageWrapper = document.querySelector('.image-wrapper');
+// window.imageValues.then( response => console.log(response));
 
-
-// function createImageSimple(imageSource = imageSource){
-//   const image = document.createElement('img');
-
-//   image.src = imageSource;
+// setTimeout(()=>{
   
-//   return image;
-// }
 
-// const dataURIFromCanvas = s_canvasImage.toDataURL('image/jpeg', 0.9);
-
-// console.log(dataURIFromCanvas);
-// const secondImage = createImageSimple(dataURIFromCanvas);
-// console.log(secondImage);
-// s_imageWrapper.appendChild(secondImage);
-
-
-
-s_canvasImage.toBlob(blob => {
-  const newImage = document.createElement('img');
-  const blobURL = URL.createObjectURL(blob);
+  const s_canvasImage = document.querySelector('.canvas-image');
+  const s_imageWrapper = document.querySelector('.image-wrapper');
   
-  newImage.addEventListener('load', () => URL.revokeObjectURL(blobURL) );
-
-  newImage.src = blobURL;
-  s_imageWrapper.appendChild(newImage);
+  // const width="1415";
+  const height="2122"
   
-  console.log(s_imageWrapper);
-});
+
+  /**
+  * Convert canvas element to blob
+  */
+  s_canvasImage.toBlob(blob => {
+    const newImage = document.createElement('img');
+    const blobURL = URL.createObjectURL(blob);
+
+    newImage.addEventListener('load', e => {
+      console.log(e);
+      URL.revokeObjectURL(blobURL) 
+    });
+
+    // newImage.width = width;
+    // newImage.height = height;
+
+    newImage.src = blobURL;
+    s_imageWrapper.appendChild(newImage);
+
+    console.log(s_imageWrapper);
+  });
+
+
+  /**
+  * creates image element
+  * @param {string} - imageSource uri to image or data uri
+  */
+  function createImageSimple(imageSource = imageSource){
+    const image = document.createElement('img');
+    
+    image.setAttribute('crossorigin' ,'');
+    // image.width = width;
+    // image.height = height;
+    image.src = imageSource;
+    return image;
+  }
+
+
+  /**
+  * take canvas element and convert to data URI
+  * Write image with data uri as source back to DOM
+  */
+  function canvasToDataURI(){
+    const dataURIFromCanvas = s_canvasImage.toDataURL('image/jpeg', 1);
+    const secondImage = createImageSimple(dataURIFromCanvas);
+
+    console.log(secondImage);
+    console.log(dataURIFromCanvas);
+    s_imageWrapper.appendChild(secondImage);
+  }
+  
+  // canvasToDataURI();
+
+// },300);
